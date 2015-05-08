955b350c37aa2f3a7b69079131ecba6b3d483a9b
Obside <obsiagae@gmail.com>
2015-05-09 01:52:50 +0200

    FirstImprovement documentation

--- src/rules/Pivoting/FirstImprovement.hpp
+++ src/rules/Pivoting/FirstImprovement.hpp
@@ -3,10 +3,19 @@
 
 #include "Improvement.hpp"
 
+/**
+ * At a specific step of an algorithm with a given Instance, Permutation and
+ * score, the FirstImprovement rules tries to find the first Permutation in the
+ * given Neighbourhood which improves the instance's score.
+ */
 class FirstImprovement : public Improvement {
 public:
     FirstImprovement(const Instance& instance);
     
+    /**
+     * Finds the first Permutation in the Neighbourhood n which has
+     * a better score than the given Permutation p for the current instance.
+     */
     Permutation improve(Permutation& p, Neighbourhood& n);
 };
 
