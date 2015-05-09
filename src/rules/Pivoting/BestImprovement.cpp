37d3f986570b0d230140af2597de0018d1316281
Obside <obsiagae@gmail.com>
2015-05-09 17:41:54 +0200

    BestImprovement documentation - methods documentation in cpp

--- src/rules/Pivoting/BestImprovement.cpp
+++ src/rules/Pivoting/BestImprovement.cpp
@@ -4,6 +4,14 @@ BestImprovement::BestImprovement(const Instance& instance) :
     Improvement(instance)
 {}
 
+/**
+ * The BestImprovement rule tries to find a Permutation which improves the
+ * current instance score in comparison to the given Permutation p. It
+ * iterates over the whole given Neighbourhood n in order to find the
+ * permutation which increases the score the most (if it exists).
+ * If no improvement can be found, the p Permutation given in parameter is
+ * simply returned.
+ */
 Permutation BestImprovement::improve(Permutation& p, Neighbourhood& n) {
     if (p.score() == -1) {
         instance_.evaluate(p);
--- src/rules/Pivoting/BestImprovement.hpp
+++ src/rules/Pivoting/BestImprovement.hpp
@@ -11,15 +11,7 @@
 class BestImprovement : public Improvement {
 public:
     BestImprovement(const Instance& instance);
-    
-    /**
-     * The BestImprovement rule tries to find a Permutation which improves the
-     * current instance score in comparison to the given Permutation p. It
-     * iterates over the whole given Neighbourhood n in order to find the
-     * permutation which increases the score the most (if it exists).
-     * If no improvement can be found, the p Permutation given in parameter is
-     * simply returned.
-     */
+
     Permutation improve(Permutation& p, Neighbourhood& n);
 };
 
